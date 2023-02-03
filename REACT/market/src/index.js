e1a8a607fab038de738ca707a48d66b3ecac691f
dudrjq321 <dlwnsghks000@gmail.com>
2023-02-03 15:04:40 +0900

    .

---
+++ REACT/market/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import App from './App';
+// import App from './수업/02.2컴포넌트생성_예제';
+// import App from './수업/03.1.문법(className)'
+// import App from './수업/03.2.문법(js표현식)'
+// import App from './수업/03.3.문법(조건문)'
+// import App from './수업/03.4.문법(반복문)'
+// import App from './수업/03.5.문법(반복문2)'
+// import App from './수업/04.1.컴포넌트_여러개'
+// import App from './수업/05.1.조건부_렌더링'
+// import App from './수업/06.1.useState기본'
+// import App from './수업/06.2.예제(리스트_좋아요_버튼)'
+// import App from './수업/06.3.예제(수정버튼)'
+// import App from './수업/06.4.spread문법'
+// import App from './수업/06.5.예제(판매완료버튼)'
+// import App from './수업/06.6.예제(판매완료버튼_true_false)'
+// import App from './수업/06.7.예제(판매완료버튼_useState)'
+// import App from './수업/06.8.예제(리스트_한방에_찍기_for문)'
+// import App from './수업/06.9.예제(리스트_한방에_찍기_useState)'
+// import App from './수업/07.예제(리스트_추가)'
+// import App from './수업/08.1.모달컴포넌트'
+// import App from './수업/08.2.Modal'
+// import App from './수업/08.3.모달컴포넌트(정보넘기기)'
+// import App from './수업/09.1.요소잡기(useRef)'
+import App from './수업/10.1.뒝근마켓_최종'
+// import App from './수업'
+
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+ 
+reportWebVitals();
